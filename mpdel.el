06f7e9ab9c741f6615f29eea4f9d2af4830b9380
Damien Cassou <damien@cassou.me>
2018-03-04 09:52:36 +0100

    Change commentary section to improve melpa description

--- mpdel.el
+++ mpdel.el
@@ -24,12 +24,10 @@
 ;;; Commentary:
 
 ;; MPDel is an Emacs client for Music Player Daemon (MPD), a flexible,
-;; powerful, server-side application for playing music.  This file
-;; makes features (e.g., playlist, navigation and playback) provided
-;; by other files in the project accessible globally through the
-;; global minor mode `mpdel-mode'.  This file is also responsible for
-;; integrating the features together to provide a seamless experience
-;; (see `mpdel-setup').
+;; powerful, server-side application for playing music.  This project
+;; provides an Emacs user interface including playlists, navigation in
+;; the database and playback control.  Read the README.org file for
+;; more information.
 
 ;;; Code:
 
