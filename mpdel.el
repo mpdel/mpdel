a1e05828e3bc03679530b4cfff1306706171cb78
Damien Cassou <damien@cassou.me>
2018-06-06 14:11:54 +0200

    Release 0.6.0

--- mpdel.el
+++ mpdel.el
@@ -6,7 +6,7 @@
 ;; Keywords: multimedia
 ;; Url: https://gitlab.petton.fr/mpdel/mpdel
 ;; Package-requires: ((emacs "25.1") (libmpdel "0.7.0"))
-;; Version: 0.5.0
+;; Version: 0.6.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
